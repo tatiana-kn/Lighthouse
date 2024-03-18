f4ff914f584d068687ecfec7789edefcb6d0c6f4
tatiana-kn <123388284+tatiana-kn@users.noreply.github.com>
2024-03-18 12:46:25 +0400

    Initial Commit

---
+++ Lighthouse/LighthouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LighthouseApp.swift
+//  Lighthouse
+//
+//  Created by Tia M on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LighthouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
